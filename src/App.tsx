dff7b2e8bab6fe18bf4ca477a46c6da0a8e82af2
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-23 10:55:09 -0300

    Implement structural updates and optimizations across multiple modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from "react"
 import { api } from "./api/Api"
 import { IProfile } from "./interfaces/profile"
-
+import GitImage from "./assets/github.png"
 const initialValue: IProfile = {
   name: '',
   avatar_url: '',
@@ -25,26 +25,36 @@ function App() {
   }
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-
-      <div>
-        <form action="" onSubmit={e => findUser(e)}>
-          <label htmlFor="">USERNAME</label>
-          <input type="text" name="" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <input type="submit" value={"enviar"} />
-        </form>
-      </div>
-      {profile && (
-        <div>
-          <img src={profile.avatar_url} alt="profile_image" />
-          <span>Name: {profile.name}</span>
-
-          <span>Bio: {profile.bio}</span>
-
+      <div className="mx-auto max-w-7xl h-screen flex flex-col justify-center items-center">
+        <div className="w-6xl">
+          <div className="flex justify-center items-center gap-8">
+            <img src={GitImage} alt="image_profile" />
+            <h1 className="text-80">Perfil GitHub</h1>
+          </div>
+          <form action="" onSubmit={e => findUser(e)}>
+            <label className="input">
+              
+              <input type="search" required placeholder="Search" />
+              <button>
+                <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </g>
+                </svg>
+              </button>
+            </label>
+          </form>
         </div>
-      )}
+
+      </div>
+
     </>
   )
 }
