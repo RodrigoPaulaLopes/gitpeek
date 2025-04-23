d89037d6ed9c992da6e563ef1a0d4665908b30f6
Rodrigo Lopes <rodrigo.plopesti@gmail.com>
2025-04-23 16:07:28 -0300

    feat: refactor ProfileContent component to utilize Avatar, Content, and Description components

--- src/components/ProfileContent/Index.tsx
+++ src/components/ProfileContent/Index.tsx
@@ -1,4 +1,7 @@
 import { IProfile } from "../../interfaces/profile"
+import Avatar from "./Avatar/Index";
+import Content from "./Content/Index";
+import Description from "./Description/Index";
 
 interface IProps {
   profile: IProfile
@@ -10,19 +13,10 @@ const ProfileContent = ({ profile }: IProps) => {
   const bio = profile.bio || "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laboriosam perspiciatis laudantium iure suscipit voluptatum, dolorem, excepturi accusamus animi, vero quasi blanditiis earum! Beatae aut laboriosam incidunt reprehenderit quae sit harum.";
 
   return (
-    <div className="flex flex-col justify-center items-center lg:flex-row items-center bg-alert mx-5 lg:mx-20 lg:px-20 py-5 rounded-lg mt-8 gap-8">
-      <div>
-        <div className="avatar">
-          <div className="w-[220px] rounded-full border-4 border-title">
-            <img src={avatarUrl} alt={name} />
-          </div>
-        </div>
-      </div>
-      <div>
-        <h1 className="text-title text-center text-xl font-semibold">{name}</h1>
-        <p className="text-base text-center  font-medium text-black">{bio}</p>
-      </div>
-    </div>
+    <Content>
+      <Avatar avatarUrl={avatarUrl} name={name}/> 
+      <Description name={name} bio={bio} />
+    </Content>
   );
 };
 
